780c8e63237839915d6ce1dff9e63644369871e7
fazioandrea.f <fazioandrea.f@gmail.com>
2021-05-13 15:10:05 +0200

    inizializzazione

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font awesame -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <!-- jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- vue 2 -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.x"></script>
+    <!-- axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios@0.21.1/dist/axios.min.js"></script>
+    <style>
+        body {
+            background: purple;
+            color: white;
+        }
+        .allCard{
+            display: flex;
+            flex-wrap: wrap;
+        }
+        .card{
+            flex-basis:20%;
+            margin:1%;
+            background-color:green;
+            text-align:center;
+        }
+    </style>
+    <script>
+    </script>
+    <title>DB ospiti</title>
+</head>
+<body>
+<!-- Pagina con la lista delle stanze,
+un click porta al dettaglio della
+stanza -->
+        <div class="container"><h1>Lista delle stanze</h1>
+        
+        <?php
+        $hello='hello';
+        echo $hello.' mondo';
+        
+        
+        ?>
+        </div>
+
+</body>
+</html>
